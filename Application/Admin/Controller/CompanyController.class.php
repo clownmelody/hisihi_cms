df87a46a9f6d87c4290c844a9fa468f7d9b5d9c5
ysl <ysl90s@qq.com>
2015-09-23 12:27:45 +0800

    修改了公司模块

--- Application/Admin/Controller/CompanyController.class.php
+++ Application/Admin/Controller/CompanyController.class.php
@@ -21,7 +21,6 @@ class CompanyController extends AdminController {
 
     public function _initialize(){
         parent::_initialize();
-        $this->getMenu();
     }
 
     /**
@@ -48,90 +47,6 @@ class CompanyController extends AdminController {
         $this->display();
     }
 
-    /**
-     * 显示左边菜单，进行权限控制
-     * @author huajie <banhuajie@163.com>
-     */
-    protected function getMenu(){
-        //获取动态分类
-        $cate_auth  =   AuthGroupModel::getAuthCategories(UID);	//获取当前用户所有的内容权限节点
-        $cate_auth  =   $cate_auth == null ? array() : $cate_auth;
-        $cate       =   M('Category')->where(array('status'=>1))->field('id,title,pid,allow_publish')->order('pid,sort')->select();
-
-        //没有权限的分类则不显示
-        if(!IS_ROOT){
-            foreach ($cate as $key=>$value){
-                if(!in_array($value['id'], $cate_auth)){
-                    unset($cate[$key]);
-                }
-                //首页顶部推荐不列出
-                if($value['id'] == 47){
-                    unset($cate[$key]);
-                }
-            }
-        }
-
-        $cate           =   list_to_tree($cate);	//生成分类树
-
-        //获取分类id
-        $cate_id        =   I('param.cate_id');
-        $this->cate_id  =   $cate_id;
-
-        //是否展开分类
-        $hide_cate = false;
-        if(ACTION_NAME != 'recycle' && ACTION_NAME != 'draftbox' && ACTION_NAME != 'mydocument'){
-            $hide_cate  =   true;
-        }
-
-        //生成每个分类的url
-        foreach ($cate as $key=>&$value){
-            $value['url']   =   'Article/index?cate_id='.$value['id'];
-            if($cate_id == $value['id'] && $hide_cate){
-                $value['current'] = true;
-            }else{
-                $value['current'] = false;
-            }
-            if(!empty($value['_child'])){
-                $is_child = false;
-                foreach ($value['_child'] as $ka=>&$va){
-                    $va['url']      =   'Article/index?cate_id='.$va['id'];
-                    if(!empty($va['_child'])){
-                        foreach ($va['_child'] as $k=>&$v){
-                            $v['url']   =   'Article/index?cate_id='.$v['id'];
-                            $v['pid']   =   $va['id'];
-                            $is_child = $v['id'] == $cate_id ? true : false;
-                        }
-                    }
-                    //展开子分类的父分类
-                    if($va['id'] == $cate_id || $is_child){
-                        $is_child = false;
-                        if($hide_cate){
-                            $value['current']   =   true;
-                            $va['current']      =   true;
-                        }else{
-                            $value['current'] 	= 	false;
-                            $va['current']      =   false;
-                        }
-                    }else{
-                        $va['current']      =   false;
-                    }
-                }
-            }
-        }
-        $this->assign('nodes',      $cate);
-        $this->assign('cate_id',    $this->cate_id);
-
-        //获取面包屑信息
-        $nav = get_parent_category($cate_id);
-        $this->assign('rightNav',   $nav);
-
-        //获取回收站权限
-        $show_recycle = $this->checkRule('Admin/article/recycle');
-        $this->assign('show_recycle', IS_ROOT || $show_recycle);
-        //获取草稿箱权限
-        $this->assign('show_draftbox', C('OPEN_DRAFTBOX'));
-    }
-
     public function add(){
         $this->display();
     }
@@ -141,7 +56,14 @@ class CompanyController extends AdminController {
             $model = M('Company');
             $cid = $_POST["cid"];
             $data["name"] = $_POST["name"];
-            $data["content"] = $_POST["content"];
+            $data["city"] = $_POST["city"];
+            $data["slogan"] = $_POST["slogan"];
+            $data["introduce"] = $_POST["introduce"];
+            $data["marks"] = $_POST["marks"];
+            $data["scale"] = $_POST["scale"];
+            $data["website"] = $_POST["website"];
+            $data["fullname"] = $_POST["fullname"];
+            $data["location"] = $_POST["location"];
             $data["picture"] = $_POST["picture"];
             $data["create_time"] = time();
             if(empty($cid)){
