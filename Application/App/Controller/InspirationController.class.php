d654cce9e4e1c0ade90c1b0b65289360b7321a27
yangchujie <yangchujie1@163.com>
2015-10-20 11:49:42 +0800

    新增灵感模块部分接口，修复官网下载链接问题

--- Application/App/Controller/InspirationController.class.php
+++ Application/App/Controller/InspirationController.class.php
@@ -31,11 +31,122 @@ class InspirationController extends AppController {
         $data_1['value'] = "特别推荐";
         $data_2['id'] = 2;
         $data_2['value'] = "精选作品";
-        $tags = array($data_1, $data_2);
+        $data_3['id'] = 3;
+        $data_3['value'] = "收藏最多";
+        $data_4['id'] = 4;
+        $data_4['value'] = "浏览最多";
+        $tags = array($data_1, $data_2, $data_3, $data_4);
         $extra['data'] = $tags;
         $this->apiSuccess('获取筛选条件列表成功', null, $extra);
     }
 
+    /**
+     * 获取灵感图片列表
+     * @param int $category_id
+     * @param int $filter_type   1-特别推荐  2-精选作品  3-收藏最多  4-浏览最多
+     * @param int $page
+     * @param int $count
+     */
+    public function inspirationImgList($category_id=0, $filter_type=1, $page=1, $count=10){
+        $model = D('Admin/Inspiration');
+        $map['status'] = 1;
+        if($category_id!=0){
+            $map['category_id'] = $category_id;
+        }
+        switch($filter_type){
+            case 1:
+                $map['special'] = 1;
+                $total_count = $model->where($map)->count();
+                $list = $model->where($map)->page($page, $count)->select();
+                break;
+            case 2:
+                $map['selection'] = 1;
+                $total_count = $model->where($map)->count();
+                $list = $model->where($map)->page($page, $count)->select();
+                break;
+            case 3:
+                $total_count = $model->where($map)->count();
+                $list = $model->where($map)->order('favorite_count desc')->page($page, $count)->select();
+                break;
+            case 4:
+                $total_count = $model->where($map)->count();
+                $list = $model->where($map)->order('view_count desc')->page($page, $count)->select();
+                break;
+        }
+        $list = $this->formatList($list);
+        $extra['totalCount'] = $total_count;
+        $extra['data'] = $list;
+        $this->apiSuccess('获取灵感图片列表成功', null, $extra);
+    }
+
+    /**
+     * 收藏灵感图片
+     * @param int $uid
+     * @param int $pic_id
+     */
+    public function doFavorite($uid=0, $pic_id=0){
+        if(empty($pic_id)){
+            $this->apiError(-1, '传入图片id为空');
+        }
+        if(empty($uid)){
+            $this->requireLogin();
+            $uid = $this->getUid();
+        }
+        $favorite['appname'] = 'Inspiration';
+        $favorite['table'] = 'Inspiration';
+        $favorite['row'] = $pic_id;
+        $favorite['uid'] = $uid;
+
+        if (D('Favorite')->where($favorite)->count()) {
+            $this->apiError(-100,'您已经收藏，不能再收藏了!');
+        } else {
+            $favorite['create_time'] = time();
+            if (D('Favorite')->where($favorite)->add($favorite)) {
+                $this->apiSuccess('感谢您的支持');
+            } else {
+                $this->apiError(-101,'写入数据库失败!');
+            }
+        }
+    }
+
+    /**
+     * 判断灵感图片是否收藏
+     * @param int $uid
+     * @param int $pic_id
+     */
+    public function isFavorite($uid=0, $pic_id=0){
+        if(empty($pic_id)){
+            $this->apiError(-1, '传入图片id为空');
+        }
+        if(empty($uid)){
+            $this->requireLogin();
+            $uid = $this->getUid();
+        }
+        $favorite['appname'] = 'Inspiration';
+        $favorite['table'] = 'Inspiration';
+        $favorite['row'] = $pic_id;
+        $favorite['uid'] = $uid;
+
+        if (D('Favorite')->where($favorite)->count()) {
+            $extra['isFavorite'] = true;
+            $this->apiSuccess('查询成功', null, $extra);
+        } else {
+            $extra['isFavorite'] = false;
+            $this->apiSuccess('查询成功', null, $extra);
+        }
+    }
+
+    private function formatList($list=null){
+        if(!empty($list)){
+            foreach($list as &$inspira){
+                $pic_id = $inspira['pic_id'];
+                $inspira['pic_url'] = $this->fetchImage($pic_id);
+                unset($inspira['id']);
+                unset($inspira['status']);
+            }
+        }
+        return $list;
+    }
 
     private function fetchImage($pic_id)
     {
