c44c9c8da5f70b306f517651e26cf92ebc1eefe6
airbreak <954957945@qq.com>
2015-10-24 16:11:09 +0800

    我的老师基本分组

--- Application/Organization/Javascript/custom/main.js
+++ Application/Organization/Javascript/custom/main.js
@@ -7,15 +7,17 @@ require.config({
     baseUrl:window.urlObject.js,
     paths: {
         jquery: 'libs/jquery-1.8.2.min',
-        announcement: 'custom/announcement'
+        announcement: 'custom/announcement',
+        teacher:'custom/teacher'
     }
 });
 
 require([
     'jquery', 
-    'announcement'
+    'announcement',
+    'teacher'
 ]);
 
-require(['jquery', 'announcement'], function ($, announcement) {
-
-});
+//require(['jquery', 'announcement'], function ($, announcement) {
+//
+//});
