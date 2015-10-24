38aa7d3eab3d530116618c3e7514d7dd5780b420
airbreak <954957945@qq.com>
2015-10-24 17:38:58 +0800

    修改菜单的事件

--- Application/Organization/Javascript/custom/menus.js
+++ Application/Organization/Javascript/custom/menus.js
@@ -1,27 +1,34 @@
 ﻿define(['jquery'],function () {
 	var url = window.location.href.split('/');
 	var last = url[url.length - 1];
-	if(last == "index"){
-		$(".side-item[name='1']").addClass("active");
-	}else if(last == "basicinfo"){
-		$(".side-item[name='2']").addClass("active");
-	}else if(last == "teachers"){
-		$(".side-item[name='3']").addClass("active");
-	}else if(last == "video"){
-		$(".side-item[name='4']").addClass("active");
-	}else if(last == "studentworks"){
-		$(".side-item[name='5']").addClass("active");
-	}else if(last == "teachcondition"){
-		$(".side-item[name='6']").addClass("active");
-	}else if(last == "certification"){
-		$(".side-item[name='7']").addClass("active");
-	}else{
-		$(".side-item[name='1']").addClass("active");
-	};
+	var index=null;
+	switch (last) {
+		case "index":
+				index=1;
+			break;
+		case "basicinfo":
+				index=2;
+			break;
+		case "teachers":
+			index=3;
+			break;
+		case "video":
+			index=4;
+			break;
+		case "studentworks":
+			index=5;
+			break;
+		case "teachcondition":
+			index=6;
+			break;
+		default :
+			index=7;
+			break;
 
-    $(".side-item").bind("click", function () {
-    	$(".side-item").removeClass("active");
-    	$(this).addClass("active");
-    });
-
-})
+	}
+	(function(){
+		$(".side-item").eq(index - 1).addClass('active')
+			.parent()
+			.siblings().find('a').removeClass("active");
+	}());
+});
