ece39eb62d1f3eb3ee4cb13bafb65467dd141900
liwei <vv15802702853@163.com>
2015-10-24 17:01:17 +0800

    侧栏样式

--- Application/Organization/Javascript/custom/main.js
+++ Application/Organization/Javascript/custom/main.js
@@ -7,21 +7,17 @@ require.config({
     baseUrl:window.urlObject.js,
     paths: {
         jquery: 'libs/jquery-1.8.2.min',
-<<<<<<< HEAD
         menus: 'custom/menus',
-        announcement: 'custom/announcement'
-=======
         announcement: 'custom/announcement',
         teacher:'custom/teacher'
->>>>>>> c44c9c8da5f70b306f517651e26cf92ebc1eefe6
     }
 });
 
 require([
-<<<<<<< HEAD
     'jquery',
     'menus',
-    'announcement'
+    'announcement',
+    'teacher'
 ]);
 
 
@@ -29,13 +25,4 @@ require([
 require(['jquery', 'announcement'], function ($, announcement) {
 
 });
-=======
-    'jquery', 
-    'announcement',
-    'teacher'
-]);
 
-//require(['jquery', 'announcement'], function ($, announcement) {
-//
-//});
->>>>>>> c44c9c8da5f70b306f517651e26cf92ebc1eefe6
--- Application/Organization/Javascript/custom/menus.js
+++ Application/Organization/Javascript/custom/menus.js
@@ -1,8 +1,25 @@
 ﻿define(['jquery'],function () {
-	var url = window.location.href;
-	console.log(url);
-    $(".side-item").bind("click", function () {
+	var url = window.location.href.split('/');
+	var last = url[url.length - 1];
+	if(last == "index"){
+		$(".side-item[name='1']").addClass("active");
+	}else if(last == "basicinfo"){
+		$(".side-item[name='2']").addClass("active");
+	}else if(last == "teachers"){
+		$(".side-item[name='3']").addClass("active");
+	}else if(last == "video"){
+		$(".side-item[name='4']").addClass("active");
+	}else if(last == "studentworks"){
+		$(".side-item[name='5']").addClass("active");
+	}else if(last == "teachcondition"){
+		$(".side-item[name='6']").addClass("active");
+	}else if(last == "certification"){
+		$(".side-item[name='7']").addClass("active");
+	}else{
+		$(".side-item[name='1']").addClass("active");
+	};
 
+    $(".side-item").bind("click", function () {
     	$(".side-item").removeClass("active");
     	$(this).addClass("active");
     });
