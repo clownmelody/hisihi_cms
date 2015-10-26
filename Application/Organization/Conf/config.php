e95c777e812571320322e1bad166c72d3370bbef
airbreak <954957945@qq.com>
2015-10-26 19:20:35 +0800

    添加api地址

--- Application/Organization/Conf/config.php
+++ Application/Organization/Conf/config.php
@@ -18,6 +18,7 @@ return array(
         '__CSS__'    => __ROOT__ . '/Application/Organization/Content/css',
         '__JS__'     => __ROOT__ . '/Application/Organization/Javascript',
         '__URL__'    =>'/hisihi-cms/weborg.php',
+        '__API__'    =>'http://115.28.72.197',
 
     ),
 
--- Application/Organization/Javascript/custom/announcement.js
+++ Application/Organization/Javascript/custom/announcement.js
@@ -24,37 +24,48 @@ define(['jquery'],function () {
 
     TodayAnnoucement.prototype={
         loadData:function(){
-            var data=[{title:'习近平卡梅伦庄园会晤 在酒吧小酌吃炸鱼薯条',date:'2015.10.23',src:'http://world.huanqiu.com/exclusive/2015-10/7827489.html?from=bdwz'},
-                {title:'巡视组：17家央企均有利益输送 存链条式腐败',date:'2015.10.20',src:'http://world.huanqiu.com/exclusive/2015-10/7827489.html?from=bdwz'},
-                {title:'交通部专车新规或存法律硬伤 专家建议重制',date:'2015.10.24',src:''},
-                {title:'中英签4000亿大单 李嘉诚被指又快人一步',date:'2015.10.04',src:''},
-                {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
-                {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
-                {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
-                {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
-                {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
-                {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
-                {title:'交通部专车新规或存法律硬伤 专家建议重制',date:'2015.10.24',src:''},
-                {title:'中英签4000亿大单 李嘉诚被指又快人一步',date:'2015.10.04',src:''},
-                {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
-                {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
-                {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
-                {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
-                {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
-                {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
-                {title:'“红海”转为“血海”：小米销量下滑引发业内担忧',date:'2015.08.04',src:''},
-            ];
-            //this.getDataAsync(function(data){
-            //    data;
-            //});
-            this.showShortAnnounceInfo(data);
+            //var data=[{title:'习近平卡梅伦庄园会晤 在酒吧小酌吃炸鱼薯条',date:'2015.10.23',src:'http://world.huanqiu.com/exclusive/2015-10/7827489.html?from=bdwz'},
+            //    {title:'巡视组：17家央企均有利益输送 存链条式腐败',date:'2015.10.20',src:'http://world.huanqiu.com/exclusive/2015-10/7827489.html?from=bdwz'},
+            //    {title:'交通部专车新规或存法律硬伤 专家建议重制',date:'2015.10.24',src:''},
+            //    {title:'中英签4000亿大单 李嘉诚被指又快人一步',date:'2015.10.04',src:''},
+            //    {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
+            //    {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
+            //    {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
+            //    {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
+            //    {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
+            //    {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
+            //    {title:'交通部专车新规或存法律硬伤 专家建议重制',date:'2015.10.24',src:''},
+            //    {title:'中英签4000亿大单 李嘉诚被指又快人一步',date:'2015.10.04',src:''},
+            //    {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
+            //    {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
+            //    {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
+            //    {title:'青岛“天价虾”被宰游客将5万奖金捐重病患儿',date:'2015.10.04',src:''},
+            //    {title:'陕西夜跑女教师遇害续：警方悬赏5万缉凶',date:'2015.10.14',src:''},
+            //    {title:'湖南杀师案细节：三名少年作案后淡定上网',date:'2015.09.04',src:''},
+            //    {title:'“红海”转为“血海”：小米销量下滑引发业内担忧',date:'2015.08.04',src:''},
+            //];
+            this.getDataAsync(function(data){
+                data=JSON.parse(data);
+                this.showShortAnnounceInfo(data);
+            });
+
+
         },
+
         getDataAsync:function(callback){
             var tempObj={
                 pageIndex:this.pageIndex,
                 count:this.perPageSize
-            };
-            $.post('../../',tempObj,callback);
+                },
+            url=window.urlObject.apiUrl+'/api.php?s=/Organization/getNotice';
+            $.ajax({
+                type: "GET",
+                url: url,
+                data: null,
+                dataType: 'jsonp',
+                success: callback
+            });
+            //$.post(window.urlObject.apiUrl+'/api.php?s=/Organization/getNotice',tempObj,callback);
         },
 
         /*
