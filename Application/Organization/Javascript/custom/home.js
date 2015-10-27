04637ba86271a5cdfc848892df9c22e0092b5686
airbreak <954957945@qq.com>
2015-10-27 12:38:21 +0800

    登录

--- Application/Organization/Javascript/custom/home.js
+++ Application/Organization/Javascript/custom/home.js
@@ -6,8 +6,8 @@ $(function(){
             $('#loginForm').ajaxSubmit({
                 type:'post',
                 //url:'http://115.28.72.197/api.php?s=/Organization/login&mobile=1850755434&password=976499',
-                //url:window.urlObject.apiUrl+'/api.php?s=/Organization/login',
-                url:'http://localhost/my/welcome.php',
+                url:'http://localhost/hisihi-cms/api.php?s=/Organization/login',
+                //url:'http://localhost/my/welcome.php',
                 data:{mobile:1850755434,password:976499 },
                 success:function(data){
                     window.location.href =window.urlObject.ctl + "/Index/announcement";
