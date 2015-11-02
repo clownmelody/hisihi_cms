dd7230c90f57d43e7405562e48e11026cadb137a
airbreak <954957945@qq.com>
2015-11-02 18:47:19 +0800

    灰色按钮改成橙色

--- Application/Organization/Javascript/custom/teacher.js
+++ Application/Organization/Javascript/custom/teacher.js
@@ -345,7 +345,7 @@ define(['jquery','jqueryui','util'],function () {
                                         '</div>'+
                                     '</div>' +
                                     '<div class="addTeacherBtnRow">'+
-                                        '<input type="button" id="addTeacherCommitBtn" class="btn btn-grey" value="确定"/>' +
+                                        '<input type="button" id="addTeacherCommitBtn" class="btn brand-info" value="确定"/>' +
                                         '<label id="addTeacherCalBtn" class="btnCommitCal" title="取消">取消</label>'+
                                     '</div>' +
                             '</div>';
