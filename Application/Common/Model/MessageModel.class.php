0b6f53406b78b85c96e34d96c3a3c532abecd09f
yangchujie <yangchujie1@163.com>
2015-11-14 17:31:03 +0800

    修改消息中心

--- Application/Common/Model/MessageModel.class.php
+++ Application/Common/Model/MessageModel.class.php
@@ -90,7 +90,7 @@ class MessageModel extends Model
      * @return int
      * @auth 陈一枭
      */
-    public function sendMessage($to_uid, $content = '', $title = '您有新的消息', $url, $from_uid = 0, $type = 0, $appname = '', $apptype = '', $source_id = 0, $find_id = 0)
+    public function sendMessage($to_uid, $content = '', $title = '您有新的消息', $url, $from_uid = 0, $type = 0, $appname = '', $apptype = '', $source_id = 0, $find_id = 0, $hide=0)
     {
         if ($to_uid == is_login()) {
             return 0;
@@ -99,7 +99,7 @@ class MessageModel extends Model
         //$Addons = A("Addons://JPush/JPush");
         //$Addons->pushtouser($user,$content,$title,null);
 
-        $this->sendMessageWithoutCheckSelf($to_uid, $content, $title, $url, $from_uid, $type, $appname, $apptype, $source_id, $find_id);
+        $this->sendMessageWithoutCheckSelf($to_uid, $content, $title, $url, $from_uid, $type, $appname, $apptype, $source_id, $find_id, $hide);
     }
 
     /**
@@ -110,7 +110,7 @@ class MessageModel extends Model
      * @param $int $from_uid 发起消息的用户，根据用户自动确定左侧图标，如果为用户，则左侧显示头像
      * @param int $type 消息类型，0系统，1用户，2应用
      */
-    public function sendMessageWithoutCheckSelf($to_uid, $content = '', $title = '您有新的消息', $url, $from_uid = 0, $type = 0, $appname = '', $apptype = '', $source_id = 0, $find_id = 0)
+    public function sendMessageWithoutCheckSelf($to_uid, $content = '', $title = '您有新的消息', $url, $from_uid = 0, $type = 0, $appname = '', $apptype = '', $source_id = 0, $find_id = 0, $hide=0)
     {
         $message['to_uid'] = $to_uid;
         $message['content'] = op_t($content);
@@ -123,6 +123,7 @@ class MessageModel extends Model
         $message['source_id'] = $source_id;
         $message['apptype'] = $apptype;
         $message['find_id'] = $find_id;
+        $message['hide'] = $hide;
 
         $rs = $this->add($message);
         return $rs;
