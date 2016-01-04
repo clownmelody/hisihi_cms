a10fd6cc6ad14c7650fe6e2375d5a6eb44c1924d
jimmy <jiangjianming_m@163.com>
2016-01-04 10:52:07 +0800

    add 320px media query

--- Public/App/js/orgbasicinfo/orgbasicinfo.js
+++ Public/App/js/orgbasicinfo/orgbasicinfo.js
@@ -6,15 +6,17 @@ define(['zepto','common'],function(){
     function OrgBasicInfo($wrapper) {
         this.$wrapper = $wrapper;
         var that = this;
+        this.fillInData();
 
         //控制视频预览框的高度
         this.videoPreviewBox();
         this.locationMapBox();
+        this.$wrapper.find('#videoPreviewBox img').bind('load',this.controlPlayBtnStyle);
     }
 
     OrgBasicInfo.prototype={
 
-
+        /*机构视频预览的图片*/
         videoPreviewBox:function(){
             var $temp=this.$wrapper.find('#videoPreviewBox'),
                 w=this.$wrapper.width(),
@@ -26,6 +28,7 @@ define(['zepto','common'],function(){
             $i.css({'top':(h-ih)/2,'left':(w-iw)/2});
         },
 
+        /*地图预览的图片*/
         locationMapBox:function(){
             var $temp=this.$wrapper.find('.mainItemLocation'),
                 w=this.$wrapper.width(),
@@ -35,6 +38,20 @@ define(['zepto','common'],function(){
                 iw=$i.width();
             this.$wrapper.find('#locationMap').css('height',h);
         },
+
+        /*播放按钮图片*/
+        controlPlayBtnStyle:function(){
+            var $temp=this.$wrapper.find('#videoPreviewBox img'),
+                w=$temp.width(),
+                h=$temp.height(),
+                $i=$temp.next(),
+                ih=$i.height(),
+                iw=$i.width();
+            $i.css({'top':(h-ih)/2,'left':(w-iw)/2});
+        },
+
+        fillInData:function(){},
+
     };
 
     return OrgBasicInfo;
