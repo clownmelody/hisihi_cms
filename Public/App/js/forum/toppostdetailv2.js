f0f7c5d6e8d5e2d8b4b7ef792c5e7bcb8cf5215b
jimmy <jiangjianming_m@163.com>
2016-04-06 18:55:27 +0800

    add debug

--- Public/App/js/forum/toppostdetailv2.js
+++ Public/App/js/forum/toppostdetailv2.js
@@ -60,8 +60,10 @@ commentObj.prototype={
                     //AppFunction.showShareView(true);  //调用安卓的方法，控制分享按钮可用
                 }
             }
+            alert('进入ios前');
             else if (this.operation.ios) {
                 //如果方法存在
+                alert(typeof getUser_iOS);
                 if (typeof getUser_iOS !="undefined") {
                     userStr=getUser_iOS();//调用app的方法，得到用户的基体信息
                     this.isFromApp=true;
