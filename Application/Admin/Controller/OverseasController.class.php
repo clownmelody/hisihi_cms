91189cb29efe614602ad6e49163a3c145f255acc
yangchujie <yangchujie1@163.com>
2016-05-05 12:20:31 +0800

    添加留学部分；

---
+++ Application/Admin/Controller/OverseasController.class.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Admin\Controller;
+
+use Think\Page;
+
+/**
+ * 留学模块
+ * Class OverseasController
+ * @package Admin\Controller
+ */
+class OverseasController extends AdminController
+{
+
+    public function index(){
+        $model = M('AbroadCountry');
+        $count = $model->count();
+        $Page = new Page($count, 10);
+        $show = $Page->show();
+        $list = $model->order('create_time desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('_list', $list);
+        $this->assign('_page', $show);
+        $this->assign("total", $count);
+        $this->assign("meta_title","海外国家列表");
+        $this->display();
+    }
+
+    public function country_add(){
+        $this->display('country_add');
+    }
+
+
+
+}
