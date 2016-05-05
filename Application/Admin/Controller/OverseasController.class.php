418125ef2eee20bdf1ae4981d38c291eaf901237
yangchujie <yangchujie1@163.com>
2016-05-05 16:38:45 +0800

    添加大学CMS；

--- Application/Admin/Controller/OverseasController.class.php
+++ Application/Admin/Controller/OverseasController.class.php
@@ -121,4 +121,73 @@ class OverseasController extends AdminController
         }
     }
 
+    public function university(){
+        $model = M('AbroadUniversity');
+        $countryModel = M('AbroadCountry');
+        $count = $model->count();
+        $Page = new Page($count, 10);
+        $show = $Page->show();
+        $list = $model->order('create_time desc')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+        foreach($list as &$university){
+            $country_id = $university['country_id'];
+            $country_info = $countryModel->field('name')->where('id='.$country_id)->find();
+            $university['country'] = $country_info['name'];
+        }
+        $this->assign('_list', $list);
+        $this->assign('_page', $show);
+        $this->assign("total", $count);
+        $this->assign("meta_title", "海外大学列表");
+        $this->display();
+    }
+
+    public function university_update(){
+        if (IS_POST) { //提交表单
+            $model = M('AbroadUniversity');
+            $uid = $_POST['uid'];
+            $data['name'] = $_POST["name"];
+            $data['website'] = $_POST["website"];
+            $data['introduction'] = $_POST["introduction"];
+            $data['sia_recommend_level'] = $_POST["sia_recommend_level"];
+            $data['sia_student_enrollment_rate'] = $_POST["sia_student_enrollment_rate"];
+            $data['difficulty_of_application'] = $_POST["difficulty_of_application"];
+            $data['tuition_fees'] = $_POST["tuition_fees"];
+            $data['toefl'] = $_POST["toefl"];
+            $data['ielts'] = $_POST["ielts"];
+            $data['proportion_of_undergraduates'] = $_POST["proportion_of_undergraduates"];
+            $data['scholarship'] = $_POST["scholarship"];
+            $data['deadline_for_applications'] = $_POST["deadline_for_applications"];
+            $data['application_requirements'] = $_POST["application_requirements"];
+            $data['school_environment'] = $_POST["school_environment"];
+            $data['country_id'] = $_POST["country_id"];
+            $data['undergraduate_majors'] = $_POST["undergraduate_majors"];
+            $data['graduate_majors'] = $_POST["graduate_majors"];
+            $pic_id = $_POST["logo_url"];
+            if(!empty($pic_id)){
+                A('Organization')->uploadLogoPicToOSS($pic_id);
+                $data['logo_url'] = A('Organization')->fetchCdnImage($pic_id);
+            }
+            if(empty($uid)){
+                $data["create_time"] = time();
+                try {
+                    $model->add($data);
+                } catch (Exception $e) {
+                    $this->error($e->getMessage());
+                }
+                $this->success('添加成功', 'index.php?s=/admin/overseas/university');
+            } else {
+                $model->where('id='.$uid)->save($data);
+                $this->success('更新成功', 'index.php?s=/admin/overseas/university');
+            }
+        } else {
+            $this->display('university_add');
+        }
+    }
+
+    public function university_add(){
+        $countryModel = M('AbroadCountry');
+        $country_list = $countryModel->field('id, name')->where('status=1')->select();
+        $this->assign('_country', $country_list);
+        $this->display('university_add');
+    }
+
 }
