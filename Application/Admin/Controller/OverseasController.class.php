af8b2abb146b5175a65d9b4a841f060a42fad7a3
yangchujie <yangchujie1@163.com>
2016-05-05 14:12:51 +0800

    新增留学国家;

--- Application/Admin/Controller/OverseasController.class.php
+++ Application/Admin/Controller/OverseasController.class.php
@@ -29,6 +29,96 @@ class OverseasController extends AdminController
         $this->display('country_add');
     }
 
+    public function country_update(){
+        if (IS_POST) { //提交表单
+            $model = M('AbroadCountry');
+            $cid = $_POST['cid'];
+            $data['name'] = $_POST["name"];
+            $pic_id = $_POST["logo_url"];
+            if(!empty($pic_id)){
+                A('Organization')->uploadLogoPicToOSS($pic_id);
+                $data['logo_url'] = A('Organization')->fetchCdnImage($pic_id);
+            }
+            if(empty($cid)){
+                $data["create_time"] = time();
+                try {
+                    $model->add($data);
+                } catch (Exception $e) {
+                    $this->error($e->getMessage());
+                }
+                $this->success('添加成功', 'index.php?s=/admin/overseas/index');
+            } else {
+                $model->where('id='.$cid)->save($data);
+                $this->success('更新成功', 'index.php?s=/admin/overseas/index');
+            }
+        } else {
+            $this->display('country_add');
+        }
+    }
 
+    /**
+     * @param $id
+     */
+    public function country_edit($id){
+        if(empty($id)){
+            $this->error('参数不能为空！');
+        }
+        /*获取一条记录的详细数据*/
+        $Model = M('AbroadCountry');
+        $data = $Model->where('id='.$id)->find();
+        if(!$data){
+            $this->error($Model->getError());
+        }
+        $this->assign('country', $data);
+        $this->meta_title = '编辑留学国家';
+        $this->display();
+    }
+
+    public function cancle_hot($id, $is_hot=0){
+        if(!empty($id)){
+            $model = M('AbroadCountry');
+            $data['is_hot'] = $is_hot;
+            if(is_array($id)){
+                foreach ($id as $i){
+                    $model->where('id='.$i)->save($data);
+                }
+            } else {
+                $id = intval($id);
+                $model->where('id='.$id)->save($data);
+            }
+            $this->success('处理成功','index.php?s=/admin/overseas/index');
+        } else {
+            $this->error('未选择要处理的数据');
+        }
+    }
+
+    public function country_set_hot($id){
+        if(!empty($id)){
+            $model = M('AbroadCountry');
+            $data['is_hot'] = 1;
+            $model->where('id='.$id)->save($data);
+            $this->success('处理成功','index.php?s=/admin/overseas/index');
+        } else {
+            $this->error('未选择要处理的数据');
+        }
+    }
+
+    public function country_set_status($id, $status=-1){
+        if(!empty($id)){
+            $model = M('AbroadCountry');
+            $data['status'] = $status;
+            if(is_array($id)){
+                foreach ($id as $i){
+                    $model->where('id='.$i)->save($data);
+                }
+            } else {
+                $id = intval($id);
+                $model->where('id='.$id)->save($data);
+            }
+            $this->success('处理成功','index.php?s=/admin/overseas/index');
+        } else {
+            $this->error('未选择要处理的数据');
+        }
+    }
 
 }
