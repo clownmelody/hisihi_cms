35efd6a8f4949d4ea8fcfb6b1f98fe7be93be7db
yangchujie <yangchujie1@163.com>
2016-06-22 14:10:28 +0800

    增加分享页面入口;

--- Application/App/Controller/PromotionController.class.php
+++ Application/App/Controller/PromotionController.class.php
@@ -17,4 +17,10 @@ class PromotionController extends AppController
         $this->display('promotion_detail');
     }
 
+    public function promotion_detail_share($organization_id=0, $promotion_id=0){
+        $this->assign("organization_id", $organization_id);
+        $this->assign("promotion_id", $promotion_id);
+        $this->display('promotion_detail_share');
+    }
+
 }
