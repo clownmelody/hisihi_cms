15504091a13c61b649eb59794925c82160d6c818
yangchujie <yangchujie1@163.com>
2016-07-11 12:22:03 +0800

    修改收藏中头条的2.9链接

--- Application/App/Controller/PublicController.class.php
+++ Application/App/Controller/PublicController.class.php
@@ -329,13 +329,17 @@ class PublicController extends AppController {
             //解析并成立图片数据
             $info['img'] = $this->fetchImage($info['cover_id']);
             if((float)$version>=2.0){
-                if((float)$version>=2.7){
+                if((float)$version >=2.9){
+                    $info['content_url'] = C('HOST_NAME_PREFIX').'app.php/public/topcontent/version/2.9/type/view/id/'.$id;
+                }else if((float)$version>=2.7 && (float)$version<2.9){
                     $info['content_url'] = C('HOST_NAME_PREFIX').'app.php/public/topcontent/version/2.7/type/view/id/'.$id;
                 } else {
                     $info['content_url'] = C('HOST_NAME_PREFIX').'app.php/public/topcontent/version/2.0/type/view/id/'.$id;
                 }
                 if((float)$version>=2.4){
-                    if((float)$version>=2.7){
+                    if((float)$version >=2.9){
+                        $info['share_url'] = C('HOST_NAME_PREFIX').'app.php/public/topcontent/version/2.9/type/view/id/'.$id;
+                    }else if((float)$version>=2.7 && (float)$version<2.9){
                         $info['share_url'] = C('HOST_NAME_PREFIX').'app.php/public/topcontent/version/2.7/type/view/id/'.$id;
                     } else {
                         $info['share_url'] = C('HOST_NAME_PREFIX').'app.php/public/v2contentforshare/type/view/id/'.$id;
