a45da8706817abdfcb34ec40634b948081eaecfd
jimmy <jiangjianming_m@163.com>
2016-07-12 11:56:13 +0800

    modified share ulr in hiworks page

--- Public/App/js/hiworks/home.js
+++ Public/App/js/hiworks/home.js
@@ -931,7 +931,7 @@ define(['fx','base','myscroll','scale'],function(fx,Base,MyScroll) {
         var workObj=window.hiworks.currentWorksObj;
         var obj={
             tile: workObj.title.trim(),
-            url: window.hisihiUrlObj.link_url+'api.php?s=/Hiworks/hiworks_share/hiword_id/'+workObj.id,
+            url:workObj.baseUrl+'/Hiworks/hiworks_share/hiword_id/'+workObj.id,
             thumb: workObj.pic_url.trim(),
             description:'我在嘿设汇发现了⼀一个很棒的作业源⽂文件，居然可以直接下载'
         };
