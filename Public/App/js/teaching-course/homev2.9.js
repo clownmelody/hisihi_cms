de54653045c6f6b8502407a0015dd3322f8ed0ff
jimmy <jiangjianming_m@163.com>
2016-08-08 18:32:54 +0800

    modified org name len bug in teaching-course page

--- Public/App/js/teaching-course/homev2.9.js
+++ Public/App/js/teaching-course/homev2.9.js
@@ -240,7 +240,7 @@ define(['base','fastclick'],function(Base){
         if(cerInfo.v){
             vStyle='display:inline-block;';
         }
-        name=this.substrLongStr(name,10);
+        //name=this.substrLongStr(name,10);
         if(!logo){
             logo='http://hisihi-other.oss-cn-qingdao.aliyuncs.com/hotkeys/hisihiOrgLogo.png'
         }
@@ -253,8 +253,8 @@ define(['base','fastclick'],function(Base){
                         '<div class="right">'+
                         '<div class="org-name">'+
                             '<div class="name">'+name+'</div>'+
-                            '<span>'+cerInfo.str+'</span>'+
                         '</div>'+
+                        '<span class="cert-box">'+cerInfo.str+'</span>'+
                         '<ul class="nums-info">'+
                             '<li><span id="view-nums">'+this.transformNums(data.view_count) + '</span><span>查看</span></li>'+
                             '<li><span id="view-watch">'+this.transformNums(data.followCount) + '</span><span>关注</span></li>'+
