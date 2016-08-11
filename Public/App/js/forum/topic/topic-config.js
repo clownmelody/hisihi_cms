c5e0d03081f01a26e48b6864acb30dbf04cb3075
jimmy <jiangjianming_m@163.com>
2016-08-11 18:52:03 +0800

    add top js compress

---
+++ Public/App/js/forum/topic/topic-config.js
@@ -0,0 +1,41 @@
+/**
+ * Created by jimmy on 2016/6/13.
+ */
+
+requirejs.config({
+    baseUrl: window.hisihiUrlObj.js,
+    paths: {
+        $:'sharecommon/zepto.min',
+        fx:'sharecommon/fx_v1.1',
+        fastclick:'sharecommon/fastclick',
+        lazyloading:'sharecommon/lazyloading',
+        prefixfree:'sharecommon/prefixfree.min',
+
+        home:'forum/topic-share',
+        base:'sharecommon/base-1.1'
+    },
+    shim: {
+        $:{
+            output:'$'
+        },
+        fx:{
+            steps:['$'],
+            output:'fx'
+        },
+        lazyloading:{
+            steps:['$','fx'],
+            output:'lazyloading'
+        },
+        prefixfree:{
+            output:'prefixfree'
+        },
+    }
+});
+
+
+require(['fx','home','prefixfree'],function(fx,topic){
+    //var userAgent = window.location.href,
+    //    reg = /category\/[1-9][0-9]*/g,
+    //    id = userAgent.match(reg)[0].toString().replace(/category\//g,'');
+    window.topic = new topic();
+});
