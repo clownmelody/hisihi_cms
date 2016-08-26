259d7c8999d17f68a96a54bd1ee53e852cbe6d72
yangchujie <yangchujie1@163.com>
2016-08-26 17:14:46 +0800

    remove unuses sql

--- Application/Forum/Model/ForumModel.class.php
+++ Application/Forum/Model/ForumModel.class.php
@@ -37,23 +37,23 @@ class ForumModel extends Model
     {
         $tag='forum_list_'.serialize($map_type);
         $forum_list = S($tag);
-        $cache_time=modC('CACHE_TIME',300,'Forum');
+        //$cache_time=modC('CACHE_TIME',300,'Forum');
         if (empty($forum_list)) {
             //读取板块列表
             $forum_list = D('Forum/Forum')->where($map_type)->order('sort asc')->select();
             $forumPostModel = D('ForumPost');
-            $forumPostReplyModel = D('ForumPostReply');
-            $forumLzlReplyModel = D('ForumLzlReply');
+            //$forumPostReplyModel = D('ForumPostReply');
+            //$forumLzlReplyModel = D('ForumLzlReply');
             foreach ($forum_list as &$f) {
                 $map['status'] = 1;
                 $map['forum_id']=$f['id'];
                 $f['background'] = $f['background'] ? getThumbImageById($f['background'], 800, 'auto') : C('TMPL_PARSE_STRING.__IMG__') . '/default_bg.jpg';
                 $f['logo'] = $f['logo'] ? getThumbImageById($f['logo'], 128, 128) : C('TMPL_PARSE_STRING.__IMG__') . '/default_logo.png';
                 $f['topic_count'] = $forumPostModel->where($map)->count();
-                $post_id=$forumPostModel->where(array('forum_id'=>$f['id']))->field('id')->select();
-                $p_id=getSubByKey($post_id,'id');
-                $map['post_id']=array('in',implode(',',$p_id));
-                $f['total_count'] = $f['topic_count'] + $forumPostReplyModel->where($map)->count();
+                //$post_id=$forumPostModel->where(array('forum_id'=>$f['id']))->field('id')->select();
+                //$p_id=getSubByKey($post_id,'id');
+                //$map['post_id']=array('in',implode(',',$p_id));
+                //$f['total_count'] = $f['topic_count'] + $forumPostReplyModel->where($map)->count();
             }
             unset($f);
             S($tag, $forum_list, 600);
