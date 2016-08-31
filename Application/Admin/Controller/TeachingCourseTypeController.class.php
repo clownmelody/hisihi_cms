54529064d74f8ceef9793603f1a47d8bb20042a8
yangchujie <yangchujie1@163.com>
2016-08-31 15:55:02 +0800

    temp save

---
+++ Application/Admin/Controller/TeachingCourseTypeController.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\Controller;
+use User\Api\UserApi as UserApi;
+use Think\Hook;
+
+
+class TeachingCourseTypeController extends AdminController {
+
+    public function index(){
+        $this->display();
+    }
+
+}
