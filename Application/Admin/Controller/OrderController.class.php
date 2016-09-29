03c2f61ab98c3d7aef7d0e446e933b7ce9d9182c
yangchujie <yangchujie1@163.com>
2016-09-29 10:28:36 +0800

    modify rebate order detail

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -13,10 +13,10 @@ class OrderController extends AdminController
         $memModel = M('Member');
         $courseModel = M('OrganizationTeachingCourse');
         $rebateModel = M('Rebate');
-        $count = $model->where('status=1')->count();
+        $count = $model->where('status>=0')->count();
         $Page = new Page($count, C('LIST_ROWS'));
         $show = $Page->show();
-        $list = $model->where('status=1')->order('create_time desc')->limit($Page->firstRow.','.$Page->listRows)
+        $list = $model->where('status>=0')->order('create_time desc')->limit($Page->firstRow.','.$Page->listRows)
             ->select();
         foreach($list as &$item){
             $user_info = $memModel->field('nickname')->where('uid='.$item['uid'])->find();
@@ -60,83 +60,33 @@ class OrderController extends AdminController
         $this->display();
     }
 
-    public function rebate_add(){
-        $this->display('rebate_add');
+    public function confirm_enroll($id){
+        $model = M('Order');
+        $data['enroll_status'] = 1;
+        $model->where('id='.$id)->save($data);
+        $this->success('更新成功', 'index.php?s=/admin/order/index');
     }
 
-    public function update(){
-        if (IS_POST) { //提交表单
-            $model = M('Rebate');
-            $cid = $_POST['cid'];
-            $data['name'] = $_POST["name"];
-            $data['value'] = $_POST["value"];
-            $data['rebate_value'] = $_POST["rebate_value"];
-            $data['buy_end_time'] = strtotime($_POST["buy_end_time"]);
-            $data['use_start_time'] = strtotime($_POST["use_start_time"]);
-            $data['use_end_time'] = strtotime($_POST["use_end_time"]);
-            $data['use_condition'] = $_POST["use_condition"];
-            $data['use_method'] = $_POST['use_method'];
-            $data['use_instruction'] = $_POST['use_instruction'];
-            if(empty($data['use_condition'])){
-                $data['use_condition'] = "";
-            }
-            if(empty($data['use_method'])){
-                $data['use_method'] = "";
-            }
-            if(empty($data['use_instruction'])){
-                $data['use_instruction'] = "";
-            }
-            if(empty($cid)){
-                $data["create_time"] = time();
-                try {
-                    $model->add($data);
-                } catch (Exception $e) {
-                    $this->error($e->getMessage());
-                }
-                $this->success('添加成功', 'index.php?s=/admin/rebate/index');
-            } else {
-                $model->where('id='.$cid)->save($data);
-                /*$updateData['service_condition'] = $data['service_condition'];
-                $updateData['using_method'] = $data['using_method'];
-                $updateData['instructions_for_use'] = $data['instructions_for_use'];
-                M('TeachingCourseCouponRelation')->where('status=1 and coupon_id='.$cid)->save($updateData);*/
-                $this->success('更新成功', 'index.php?s=/admin/rebate/index');
-            }
-        } else {
-            $this->display('rebate_add');
-        }
-    }
-
-    public function edit($id){
-        if(empty($id)){
-            $this->error('参数不能为空！');
-        }
-        $Model = M('Rebate');
-        $data = $Model->where('id='.$id)->find();
-        if(!$data){
-            $this->error($Model->getError());
-        }
-        $this->assign('rebate', $data);
-        $this->meta_title = '编辑抵扣券';
-        $this->display();
+    public function confirm_gift_package($id){
+        $model = M('Order');
+        $data['gift_package_status'] = 1;
+        $model->where('id='.$id)->save($data);
+        $this->success('更新成功', 'index.php?s=/admin/order/index');
     }
 
     public function set_status($id, $status=-1){
         if(!empty($id)){
-            $model = M('Rebate');
-            /*$tccr_model = M('TeachingCourseCouponRelation');*/
+            $model = M('Order');
             $data['status'] = $status;
             if(is_array($id)){
                 foreach ($id as $i){
                     $model->where('id='.$i)->save($data);
-                    /*$tccr_model->where('coupon_id='.$i)->save(array('status'=>-1));*/
                 }
             } else {
                 $id = intval($id);
                 $model->where('id='.$id)->save($data);
-                /*$tccr_model->where('coupon_id='.$id)->save(array('status'=>-1));*/
             }
-            $this->success('处理成功','index.php?s=/admin/rebate/index');
+            $this->success('处理成功','index.php?s=/admin/order/index');
         } else {
             $this->error('未选择要处理的数据');
         }
