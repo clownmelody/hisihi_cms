cd080ef0db8db637cd4a63bd81c49b74b841557b
yangchujie <yangchujie1@163.com>
2016-10-11 18:35:07 +0800

    add func

--- Application/App/Controller/TeacherController.class.php
+++ Application/App/Controller/TeacherController.class.php
@@ -16,4 +16,74 @@ class TeacherController extends BaseController
         $this->display('teacherv3_1');
     }
 
+    public function tagList(){
+        $model = M('OrganizationTag');
+        $map['status'] = 1;
+        $map['type'] = 10;
+        $list = $model->field('id, value')->where($map)->select();
+        $extra['data'] = $list;
+        $this->apiSuccess('获取老师标签列表成功', null, $extra);
+    }
+
+    /**
+     * 创建老师
+     * @param $name
+     * @param $avatar
+     * @param $title
+     * @param $tag
+     * @param $introduce
+     * @param null $student_list
+     * @param $teach_age
+     * @param $employment_rate
+     * @param $student_num
+     * @param null $student_work_list
+     */
+    public function createTeacher($name, $avatar, $title, $tag, $introduce, $student_list=null,
+                           $teach_age, $employment_rate, $student_num, $student_work_list=null){
+        $model = M('OrganizationTeacher');
+        $tsrmodel = M('OrganizationTeacherStudentRelation');
+        $swmodel = M('StudentWorks');
+        $data['name'] = $name;
+        $data['avatar'] = $avatar;
+        $data['title'] = $title;
+        $data['tag'] = $tag;
+        $data['introduce'] = $introduce;
+        $data['teach_age'] = $teach_age;
+        $data['employment_rate'] = $employment_rate;
+        $data['student_num'] = $student_num;
+        $data['create_time'] = time();
+        $teacher_id = $model->add($data);
+        if(!empty($student_list)){
+            $student_list = json_decode($student_list);
+            foreach($student_list as $sid){
+                $tsrdata['teacher_id'] = $teacher_id;
+                $tsrdata['student_id'] = $sid;
+                $tsrdata['create_time'] = time();
+                $tsrmodel->add($tsrdata);
+            }
+        }
+        if(!empty($student_work_list)){
+            $student_work_list = json_decode($student_work_list);
+            foreach($student_work_list as $pic_url){
+                $swdata['teacher_id'] = $teacher_id;
+                $swdata['pic_url'] = $pic_url;
+                $swdata['create_time'] = time();
+                $swmodel->add($tsrdata);
+            }
+        }
+        $this->apiSuccess('创建老师成功');
+    }
+
+    public function createStudent($name, $avatar, $title, $company, $salary){
+        $model = M('OrganizationStudent');
+        $data['name'] = $name;
+        $data['avatar'] = $avatar;
+        $data['title'] = $title;
+        $data['company'] = $company;
+        $data['salary'] = $salary;
+        $data['create_time'] = time();
+        $id = $model->add($data);
+        $this->apiSuccess('创建学生成功', null, array('id'=>$id));
+    }
+
 }
