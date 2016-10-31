5964f7c85e383711eaec39113c0b9e8e9dfc2ddb
mayoi <1299824519@qq.com>
2016-10-31 15:48:19 +0800

    add baike page

---
+++ Public/App/js/baike/config.js
@@ -0,0 +1,40 @@
+/**
+ * Created by hisihi on 2016/10/31.
+ */
+requirejs.config ({
+    //give me the things that I loved when the diamond in the sky
+    baseUrl:window.hisihiUrlObj.js,
+    path: {
+        $:'sharecommon/zepto.min',
+        p:'sharecommon/fx_v1.1',
+        base:'sharecommon/base-1.1',
+        fastclick:'sharecommon/fastclick',
+        prefixfree:'sharecommon/prefixfree',
+        home:'baike/baike',
+        //约定不需要具体网络数据获取情况
+    },
+    shim: {
+        $: {
+            output: '$'
+        },
+        fx: {
+            steps: ['$'],
+            output: 'fx'
+        },
+        prefixfree: {
+            output: 'prefixfree'
+        },
+    }
+});
+
+//正则表达式获取当前页面的id和网络地址
+require(['home','prefixfree'],function(Baike) {
+    var url = window.location.href;
+    if (url.indexOf('%2f') > 0) {
+        url=url.replace(/\%2F/g,'\/');
+    }
+
+    var reg = /id\/[0-9][0-9]*/g,
+        id = url.match(reg)[0].toString().replace(/id\//g,'');
+    window.Baike = new Baike(id,window.hisihiUrlObj.api_url_php);
+});
